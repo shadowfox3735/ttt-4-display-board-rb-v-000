b52427661c3936a2010501cd84357b40392b097f
shadowfox3735 <normanpan180@gmail.com>
2018-10-10 20:56:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,10 +7,10 @@ def display_board(board)
   
   puts "-----------"
   
-  puts "  #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   
   puts "-----------"
   
-  puts "  #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
   
 end
